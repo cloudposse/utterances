ba8ddc6bc52774257908c1906b0dd825137be4eb
Aliaksandr Babai <aliaksandr.babai@gmail.com>
2018-07-19 14:41:03 +0300

    refactor(*): change paths for static and API

--- src/client.ts
+++ src/client.ts
@@ -5,7 +5,7 @@ let script = document.currentScript as HTMLScriptElement;
 if (script === undefined) {
   // Internet Explorer :(
   // tslint:disable-next-line:max-line-length
-  script = document.querySelector('script[src^="https://utteranc.es/client.js"],script[src^="http://localhost:4000/client.js"]') as HTMLScriptElement;
+  script = document.querySelector('script[src^="utterances/client.js"]"]') as HTMLScriptElement;
 }
 
 // gather script element's attributes
@@ -48,7 +48,7 @@ document.head.insertAdjacentHTML(
   </style>`);
 
 // create the comments iframe and it's responsive container
-const utterancesOrigin = script.src.match(/^https:\/\/utteranc.es|http:\/\/localhost:\d+/)![0];
+const utterancesOrigin = 'utterances'
 const url = `${utterancesOrigin}/utterances.html`;
 script.insertAdjacentHTML(
   'afterend',
